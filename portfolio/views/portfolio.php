e8deb18b8d3c3b7dd733b2ba4a204abe70b8cc26
Travis Hoki <hokirocko@gmail.com>
2015-07-20 22:20:10 -0600

    add website filters

--- portfolio/views/portfolio.php
+++ portfolio/views/portfolio.php
@@ -3,15 +3,20 @@
 	<?php include '../inc/portfolio_menu.php';?>
 
 	<div class="site-filters">
+		<h2>Filters</h2>
 		<div class="centeredmenu">
 			<ul class="techs">
-				<li ng-repeat="tech in site_filters" ng-class="tech" class="dark" title="{{tech}}"></li>
+				<li ng-repeat="tech in site_filters" 
+				ng-class="filterStyle(tech)" 
+				class="filters" 
+				title="{{tech.title}}" 
+				ng-click="clickFilter(tech)"></li>
 			</ul>
 		</div>
 	</div>	
 
 	<ul class="site-gallery">
-		<li ng-repeat="site in sites" 
+		<li ng-repeat="site in sites | techFilter : site_filters" 
 		class="col-xs-6 col-sm-4 col-md-4 col-lg-4"
 		ng-click="openModal($event, $index, site)" 
 		ng-class="{full: site.full, item: !site.full, active: site.active}"
