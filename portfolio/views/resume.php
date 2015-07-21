5e2c1269b23d80789cf25cd820d1b0043721cc0b
Travis Hoki <hokirocko@gmail.com>
2015-07-21 01:38:00 -0600

    resume-page-clean-and-style

--- portfolio/views/resume.php
+++ portfolio/views/resume.php
@@ -33,39 +33,61 @@
         </ul> 
         
         <h1>Experience</h1>
-        <ul>
+        <ul class="experience">
             <li>
-                <h2>Enspark</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Orem, UT</p>
-                <p class="date"> Nov 2011 &minus; Present</p>
-                <p class="job-position">Custom Developer</p>
-                <ul>
-                    <li>PHP and JavaScript development</li>
-                    <li>LMS reconstruction and user account tracking</li>
-                    <li>Front-end development and WordPress maintenance</li>
-                    <li>Custom banner and eLearning course development in Adobe Flash AS3</li>
-                </ul>
+                <img src="portfolio/imgs/experience/roomchoice.png" title="Roomchoice">
+                <div class="experience-content">
+                    <h2>Room Choice</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>American Fork, UT</p>
+                    <p class="date"> Dec 2014 &minus; Present</p>
+                    <p class="job-position">Full Stack Web-App Developer</p>
+                    <ul>
+                        <li>Python and Djang</li>
+                        <li>Angular, CoffeeScript, Classy</li>
+                        <li>In house developement on student housing software</li>
+                    </ul>
+                </div>
             </li>
             <li>
-                <h2>All American Sod Farms</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Orem, UT</p>
-                <p class="date">Jun 2011 &minus; Jun 2012</p>
-                <p class="job-position">Office Manager / Web Master</p>
-                <ul>
-                    <li>Creating and maintaining AllAmericanSod.com</li>
-                    <li>Scheduling and dispatching deliveries</li>
-                    <li>Bill collecting and register operation</li>
-                    <li>Customer service and clerical work</li>
-                </ul>
+                <img src="portfolio/imgs/experience/enspark.png" title="Enspark">
+                <div class="experience-content">
+                    <h2>Enspark</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Lehi, UT</p>
+                    <p class="date"> Nov 2011 &minus; Dec 2014</p>
+                    <p class="job-position">Custom Developer</p>
+                    <ul>
+                        <li>PHP and JavaScript development</li>
+                        <li>LMS reconstruction and user account tracking</li>
+                        <li>Front-end development and WordPress maintenance</li>
+                        <li>Custom banner and eLearning course development in Adobe Flash AS3</li>
+                    </ul>
+                </div>
             </li>
             <li>
-                <h2>Wendy's</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Orem, UT</p>
-                <p class="date">Dec 2006 &minus; Jun 2011</p>
-                <p class="job-position">Crew Member</p>
-                <ul>
-                    <li>Register operation</li>
-                    <li>Workstation sanitation and stocking</li>
-                    <li>Train new employees in various positions</li>
-                    <li>Maintain service and courtesy standards during stressful situations</li>
-                </ul>
+                <img src="portfolio/imgs/experience/all-american-sod-farms.png" title="All American Sod Farms">
+                <div class="experience-content">
+                    <h2>All American Sod Farms</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Orem, UT</p>
+                    <p class="date">Jun 2011 &minus; Jun 2012</p>
+                    <p class="job-position">Office Manager / Web Master</p>
+                    <ul>
+                        <li>Creating and maintaining AllAmericanSod.com</li>
+                        <li>Scheduling and dispatching deliveries</li>
+                        <li>Bill collecting and register operation</li>
+                        <li>Customer service and clerical work</li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <img src="portfolio/imgs/experience/wendys.png" title="Wendy's">
+                <div class="experience-content">
+                    <h2>Wendy's</h2><p class="location"><span class="tabletVisible desktopVisible fleft">,&nbsp;</span>Orem, UT</p>
+                    <p class="date">Dec 2006 &minus; Jun 2011</p>
+                    <p class="job-position">Crew Member</p>
+                    <ul>
+                        <li>Register operation</li>
+                        <li>Workstation sanitation and stocking</li>
+                        <li>Train new employees in various positions</li>
+                        <li>Maintain service and courtesy standards during stressful situations</li>
+                    </ul>
+                </div>
             </li>
         </ul>
     
