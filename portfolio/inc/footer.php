6d5433b0a8b8f10c708e4667b63a41b4784c4a96
Travis Hoki <hokirocko@gmail.com>
2015-08-11 21:48:43 -0600

    adding to home page, cleaning resume page

--- portfolio/inc/footer.php
+++ portfolio/inc/footer.php
@@ -1,12 +1,25 @@
-<div id="footer-holder">
-    <div id="footer">
+<div class="above-footer">
+	<div class="above-footer-inner">
+        <ul class="social-icons">
+            <li>
+                <a href="https://github.com/travishoki/" target="_blank" title="Travis's Github">
+                    <i class="fa fa-git"></i>
+                </a>
+            </li>    
+            <li>
+                <a href="https://www.linkedin.com/pub/travis-hoki/85/b0b/113" target="_blank" title="Travis's Linked In">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>    
+        </ul>
+	</div>
+</div>
+<div id="footer">
+    <div class="footer-inner">
         <p>
             <span>Copyright Travishoki.com <?php echo date('Y');?></span> 
-            <br  class="phoneOnly"/>
-            <span class="tabletVisible desktopVisible">&nbsp;&minus;&nbsp;</span>
             <span>All Rights Reserved</span>
         </p>
-        
-        <p class="backToTop" ng-click="backToTop()">Back to Top</p>
-    </div><!-- #footer -->
+    	<i class="fa fa-angle-up back-to-top"  ng-click="backToTop()"></i>
+	</div><!-- #footer -->
 </div><!-- #footer -->
