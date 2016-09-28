c2d8471b6e98a42fc66351d6d98aef07aeb53089
Travis Hoki <travis.hoki@gmail.com>
2016-09-28 00:31:12 -0600

    add grunt, add phploy

--- portfolio/src/js/controllers/home.js
+++ portfolio/src/js/controllers/home.js
@@ -1,12 +1,12 @@
 function HomeCtrl($scope, $http){
 	
 	$scope.init = function(){
-		$scope.getIcons();
+		$scope.iconArray = $scope.getIcons();
 	};//init
 
 	/*------------------------------ Get Icons ------------------------------*/
 	$scope.getIcons = function(){
-		$scope.iconArray = ['JavaScript','HTML','CSS','jQuery','AngularJs','WordPress','Python','PHP','LESS-CSS','Coffee-Script'];
+		return ['JavaScript','HTML','CSS','jQuery','AngularJs','WordPress','Python','PHP','LESS-CSS','Coffee-Script'];
 	};
 	
 	/*------------------------------ INIT ------------------------------*/
